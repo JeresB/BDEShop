c8852a46de6a7329774e81e950f6ed841cd9d6e9
JeresB <boiselet.jeremy@gmail.com>
2019-03-25 11:49:55 +0100

    update

--- test.php
+++ test.php
@@ -4,6 +4,6 @@ $root = realpath($_SERVER["DOCUMENT_ROOT"]);
 require_once("$root/admin/modele/factures.php");
 session_start();
   $gestion_bdd = new BDD_FACTURES();
-print('<pre>'.print_r($gestion_bdd->getEmail($order_ref), true).'</pre>');
+print('<pre>'.print_r($gestion_bdd->getEmail(4), true).'</pre>');
 
 ?>
